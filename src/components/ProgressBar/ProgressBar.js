d155e636c2df745ca3d4b68837bc8d97f2819bad
Sam <lenry101@gmail.com>
2023-05-25 23:41:46 +0100

    feat: add progress bar code

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,27 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Wrapper = styled.div`
+  box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
+  background-color: ${COLORS.transparentGray15};
+  width: 370px;
+  height: ${props => props.size === 'large' ? '24px' : props.size === 'medium' ? '12px' : '8px'};
+  border-radius: 8px;
+  padding: ${props => props.size === 'large' ? '4px' : '0px'};
+`
+
+const Progress = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${props => props.value + '%'};
+  height: 100%;
+  border-radius: ${props => props.value >= 99 ? '4px' : '4px 0px 0px 4px'}; 
+`
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+      <Wrapper size={size}>
+        <Progress value={value}/>
+      </Wrapper>);
 };
 
 export default ProgressBar;
