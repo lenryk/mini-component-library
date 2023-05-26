a258a78d45c2f0b53ec8619393735aecd5ac6dce
Sam <lenry101@gmail.com>
2023-05-27 00:27:34 +0100

    feat: new styled select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,67 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
+
+const CustomSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: 1rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: 24px;
+  height: 24px;
+  pointer-events: none;
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+      <Wrapper>
+        <NativeSelect id={label} value={value} onChange={onChange}>
+          {children}
+        </NativeSelect>
+        <CustomSelect>
+          {displayedValue}
+          <IconWrapper size={24}>
+            <Icon id="chevron-down" strokeWidth={1} size={24}/>
+          </IconWrapper>
+        </CustomSelect>
+      </Wrapper>
+
   );
 };
 
